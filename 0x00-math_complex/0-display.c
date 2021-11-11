502b5765b24fed95e1ebb11c26db4de16912711c
wolf-coder <cuore.fakhri@gmail.com>
2021-11-11 19:36:45 +0100

    Fix negative cmplex display

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -8,16 +8,17 @@
 
 void display_complex_number(complex c)
 {
+
 	if (c.re)
 	{
-		printf("%.0f", c.re);
+		printf("%.9g", c.re);
 		if (c.im)
-			printf(" + %.0fi", c.im);
+			(c.im > 0) ? (printf(" + %.gi", c.im)) : printf(" - %.gi", (-1) * c.im);
 	}
 	else
 	{
 		if (c.im)
-			printf("%.0fi", c.im);
+			printf("%.gi", c.im);
 		else
 			printf("0");
 	}
