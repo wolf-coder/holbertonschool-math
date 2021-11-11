50746b5c95c584d24373367bb471c3f01638123b
wolf-coder <cuore.fakhri@gmail.com>
2021-11-11 19:47:05 +0100

    fixing Img cplx number = 1

--- 0x00-math_complex/0-display.c
+++ 0x00-math_complex/0-display.c
@@ -13,7 +13,12 @@ void display_complex_number(complex c)
 	{
 		printf("%.9g", c.re);
 		if (c.im)
-			(c.im > 0) ? (printf(" + %.gi", c.im)) : printf(" - %.gi", (-1) * c.im);
+		{
+			if (c.im != 1 && c.im != (-1))
+				(c.im > 0) ? (printf(" + %.gi", c.im)) : printf(" - %.gi", (-1) * c.im);
+			else
+				(c.im > 0) ? (printf(" + i")) : printf(" - i");
+		}
 	}
 	else
 	{
