764928d478c4eb96187b2254b19e1576474988a5
wolf-coder <cuore.fakhri@gmail.com>
2021-11-12 12:32:29 +0100

    adding modulus function

--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -2,6 +2,8 @@
 #define Func_h
 
 #include <stdio.h>
+#include <math.h>
+
 /**
  * struct complex - Structure to represent a complex number.
  * @re: Real part of the complex number.
@@ -16,4 +18,6 @@ typedef struct complex complex;
 
 void display_complex_number(complex c);
 complex conjugate(complex c);
+double modulus(complex c);
+
 #endif
