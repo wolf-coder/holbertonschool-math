0571f2de94e7d9d9e5b4e48243611df1119315d7
wolf-coder <cuore.fakhri@gmail.com>
2021-11-13 10:21:35 +0100

    fix betty

--- 0x00-math_complex/6-multiplication.c
+++ 0x00-math_complex/6-multiplication.c
@@ -1,5 +1,12 @@
 #include "holberton.h"
-
+/**
+ * multiplication - function that performs the multiplication operation to
+ **complex numbers.
+ * @c1: The given complex number.
+ * @c2: The given complex number.
+ * @c3: the resulted Complex number .
+ * Return - Void.
+ **/
 void multiplication(complex c1, complex c2, complex *c3)
 {
 	c3->re = (c1.re * c2.re) - (c1.im * c2.im);
