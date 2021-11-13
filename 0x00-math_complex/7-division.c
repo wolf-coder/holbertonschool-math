3dc524b69a489746d24de75c36de26df73c382a8
wolf-coder <cuore.fakhri@gmail.com>
2021-11-13 11:26:11 +0100

    division func

--- 0x00-math_complex/7-division.c
+++ 0x00-math_complex/7-division.c
@@ -10,6 +10,7 @@
 
 void division(complex c1, complex c2, complex *c3)
 {
-	c3->re = (c1.re * c2.re + c1.im * c2.im) / c2.re * c2.re + c2.im * c2.im;
-	c3->im = (-c1.re * c2.im + c1.im * c2.re) / c2.re * c2.re + c2.im * c2.im;
+	c3->re = (c1.re * c2.re + c1.im * c2.im) / (c2.re * c2.re + c2.im * c2.im);
+	c3->im = (-c1.re * c2.im + c1.im * c2.re) /
+		(c2.re * c2.re + c2.im * c2.im);
 }
